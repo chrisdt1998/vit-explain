08cf2f44c25df48f5f35e2109a11f1e45085b850
chrisdt1998 <chris.dutoit98@gmail.com>
2021-12-10 10:06:48 +0100

    Edits for LRP method made.

--- vit_grad_rollout.py
+++ vit_grad_rollout.py
@@ -11,6 +11,7 @@ def grad_rollout(attentions, gradients, discard_ratio):
     with torch.no_grad():
         for attention, grad in zip(attentions, gradients):                
             weights = grad
+            #Rule 5 from MM TIBAV paper. ~ C
             attention_heads_fused = (attention*weights).mean(axis=1)
             attention_heads_fused[attention_heads_fused < 0] = 0
 
@@ -23,28 +24,28 @@ def grad_rollout(attentions, gradients, discard_ratio):
 
             I = torch.eye(attention_heads_fused.size(-1))
             a = (attention_heads_fused + 1.0*I)/2
-            a = a / a.sum(dim=-1)
+            for i in range(a.shape[0]):
+                a[i] = a[i] / a[i].sum(dim=-1)
             result = torch.matmul(a, result)
-    
+
     # Look at the total attention between the class token,
     # and the image patches
-    mask = result[0, 0 , 1 :]
+    mask = result[:, 0, 1 :]
     # In case of 224x224 image, this brings us from 196 to 14
     width = int(mask.size(-1)**0.5)
-    mask = mask.reshape(width, width).numpy()
+    mask = mask.reshape(mask.shape[0], width, width).numpy()
     mask = mask / np.max(mask)
     return mask    
 
 class VITAttentionGradRollout:
-    def __init__(self, model, attention_layer_name='attn_drop',
+    def __init__(self, model, attention_layer_name='attn.attn_drop',
         discard_ratio=0.9):
         self.model = model
         self.discard_ratio = discard_ratio
         for name, module in self.model.named_modules():
-            if attention_layer_name in name:
+            if attention_layer_name in name and 'temporal_attn' not in name:
                 module.register_forward_hook(self.get_attention)
-                module.register_backward_hook(self.get_attention_gradient)
-
+                module.register_full_backward_hook(self.get_attention_gradient)
         self.attentions = []
         self.attention_gradients = []
 
@@ -54,13 +55,15 @@ class VITAttentionGradRollout:
     def get_attention_gradient(self, module, grad_input, grad_output):
         self.attention_gradients.append(grad_input[0].cpu())
 
-    def __call__(self, input_tensor, category_index):
+    def __call__(self, input_tensor, category_index=None):
         self.model.zero_grad()
         output = self.model(input_tensor)
-        category_mask = torch.zeros(output.size())
+        pred_label = np.argmax(output.cpu().data.numpy(), axis=-1)
+        category_index = np.argmax(output.cpu().data.numpy(), axis=-1)[0]
+        category_mask = torch.zeros(output.size()).cuda()
         category_mask[:, category_index] = 1
         loss = (output*category_mask).sum()
         loss.backward()
 
         return grad_rollout(self.attentions, self.attention_gradients,
-            self.discard_ratio)
+            self.discard_ratio), pred_label
--- vit_rollout.py
+++ vit_rollout.py
@@ -28,27 +28,28 @@ def rollout(attentions, discard_ratio, head_fusion):
 
             I = torch.eye(attention_heads_fused.size(-1))
             a = (attention_heads_fused + 1.0*I)/2
-            a = a / a.sum(dim=-1)
+            for i in range(a.shape[0]):
+                a[i] = a[i] / a[i].sum(dim=-1)
 
             result = torch.matmul(a, result)
     
     # Look at the total attention between the class token,
     # and the image patches
-    mask = result[0, 0 , 1 :]
+    mask = result[:, 0 , 1 :]
     # In case of 224x224 image, this brings us from 196 to 14
     width = int(mask.size(-1)**0.5)
-    mask = mask.reshape(width, width).numpy()
+    mask = mask.reshape(mask.shape[0], width, width).numpy()
     mask = mask / np.max(mask)
     return mask    
 
 class VITAttentionRollout:
-    def __init__(self, model, attention_layer_name='attn_drop', head_fusion="mean",
+    def __init__(self, model, attention_layer_name='attn.attn_drop', head_fusion="mean",
         discard_ratio=0.9):
         self.model = model
         self.head_fusion = head_fusion
         self.discard_ratio = discard_ratio
         for name, module in self.model.named_modules():
-            if attention_layer_name in name:
+            if attention_layer_name in name and 'temporal_attn' not in name:
                 module.register_forward_hook(self.get_attention)
 
         self.attentions = []
@@ -60,5 +61,6 @@ class VITAttentionRollout:
         self.attentions = []
         with torch.no_grad():
             output = self.model(input_tensor)
+        pred_label = np.argmax(output.cpu().data.numpy(), axis=-1)
 
-        return rollout(self.attentions, self.discard_ratio, self.head_fusion)
+        return rollout(self.attentions, self.discard_ratio, self.head_fusion), pred_label
